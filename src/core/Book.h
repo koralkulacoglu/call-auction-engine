0fcea3ab59476cabbc4a9915adc572fed8f59704
Koral Kulacoglu <kulacoglukoral@gmail.com>
2026-08-11 15:23:04 -0700

    feat: added Book class

---
+++ src/core/Book.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Types.h"
+
+#include <map>
+#include <vector>
+
+namespace cae {
+
+class Book {
+public:
+  void insert(const Order &order);
+
+  std::vector<LadderRow> ladder() const;
+
+  std::optional<Price> bestBid() const;
+  std::optional<Price> bestAsk() const;
+
+private:
+  std::map<Price, std::vector<Order>, std::greater<Price>> bids;
+  std::map<Price, std::vector<Order>, std::less<Price>> asks;
+};
+
+} // namespace cae
